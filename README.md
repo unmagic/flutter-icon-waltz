387bbfe8191be6609d67e9b3c2e749e267d1bcdc
liubiao <liubiao@vlifepaper.com>
2018-11-02 15:06:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# icon_waltz
+
+A Flutter package that provides beautiful common Icons(SVG) .
+This Project is under
+
+## Getting Started
+
+For help getting started with Flutter, view our online [documentation](https://flutter.io/).
+
+For help on editing package code, view the [documentation](https://flutter.io/developing-packages/).
---
+++ lib/icon_waltz.dart
@@ -0,0 +1,20 @@
+library icon_waltz;
+
+import 'package:flutter/widgets.dart';
+
+/// Identifiers for the beautiful icons named GrandWaltz.
+/// Use with the [Icon] class to show specific icons
+/// Icons are identified by their name as listed below.
+/// To use this class, For example:
+///
+/// ```your pubspec.yaml
+/// dependencies:
+///  waltz_icon:
+///    git:
+///     url: git://github.com:unmagic/flutter-icon-waltz.git
+/// ```
+///
+class WaltzIcons {
+  ///<i class="material-icons md-36">360</i> &#x2014; material icon named "360".
+  static const IconData clear = IconData(0xe601, fontFamily: 'GrandWaltzIcons');
+}
